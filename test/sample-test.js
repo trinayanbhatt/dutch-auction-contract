fd25a31a4886c11c02202df8c3bb25da645496e0
Trinayan Bhatt <Bhatt_t@India.XoriantCorp.com>
2022-02-06 12:54:19 +0530

    modified test case

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,17 +1,14 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("NFTMarket", function () {
+describe("DutchAuction", function () {
   it("Should create and execute dutch auction", async function () {
     const NFT = await hre.ethers.getContractFactory("ERC721");
     const nft = await NFT.deploy();
     await nft.deployed();
     const nftAddress = nft.address
 
-    await nft.mint("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", 101)
-
     console.log("NFT contract deployed to:", nft.address);
-
     
     const DuctchAuction = await hre.ethers.getContractFactory("DutchAuction");
     const ductchAuction = await DuctchAuction.deploy(1000000, 1, nftAddress, 101);
